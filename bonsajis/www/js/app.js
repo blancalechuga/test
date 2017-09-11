91688098241c9e14a7c120081edef4b1fea22ed9
blancalechuga <blanca_lechuga.carrizales@hotmail.com>
2017-09-11 10:47:52 -0600

    bonsais

--- bonsajis/www/js/app.js
+++ bonsajis/www/js/app.js
@@ -1,3 +1,12 @@
-new Rect(10,10,100,100)
+/*new Rect(10,10,100,100)
 .addTo(stage)
 .attr(`fillColor`,`green`)
+.animate(new KeyframeAnimation('2s',{
+    from: {x: 0, y:0},
+    '0.5%':{x:300, y:300},
+    to:{x:0, y:0}
+}));*/
+
+new Circle(500, 500, 100, 100)
+.addTo(stage)
+.attr('fillColor', 'yellow');
